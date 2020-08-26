97c6f70e76e0e6581e144e24f9932fcc4fb8d6a8
jx95 <jx95@duke.edu>
2020-08-26 17:42:28 -0400

    add segmented controls

--- HW1.playground/Contents.swift
+++ HW1.playground/Contents.swift
@@ -111,6 +111,16 @@ func addOrUpdatePerson(firstName: String, lastName: String, whereFrom: String, g
 //print(people_list)
 
 class HW1ViewController : UIViewController {
+    
+    let first_input = UITextField()
+    let last_input = UITextField()
+    let from_input = UITextField()
+    let id_input = UITextField()
+    
+    var selectedGender : Gender?
+    var selectedRole : DukeRole?
+    var selectedProgram : DukeProgram?
+    
     override func loadView() {
 // You can change color scheme if you wish
         let view = UIView()
@@ -125,32 +135,143 @@ class HW1ViewController : UIViewController {
 // You can add code here
         // First Name
         let first_label = UILabel()
-        first_label.frame = CGRect(x: 40, y: 60, width: 200, height: 20)
+        first_label.frame = CGRect(x: 40, y: 60, width: 200, height: 25)
         first_label.text = "First Name"
         first_label.textColor = .white
         view.addSubview(first_label)
         
+        //let first_input = UITextField()
+        first_input.frame = CGRect(x: 150, y: 60, width: 180, height: 25)
+        first_input.backgroundColor = .black
+        first_input.textColor = .white
+        first_input.layer.borderWidth = 1
+        first_input.layer.borderColor = UIColor.white.cgColor
+        first_input.layer.cornerRadius = 4.0
+        //first_input.resignFirstResponder()
+        view.addSubview(first_input)
+        
+        // last name
         let last_label = UILabel()
-        last_label.frame = CGRect(x: 40, y: 110, width: 200, height: 20)
+        last_label.frame = CGRect(x: 40, y: 110, width: 200, height: 25)
         last_label.text = "Last Name"
         last_label.textColor = .white
         view.addSubview(last_label)
         
+        //let last_input = UITextField()
+        last_input.frame = CGRect(x: 150, y: 110, width: 180, height: 25)
+        last_input.backgroundColor = .black
+        last_input.textColor = .white
+        last_input.layer.borderWidth = 1
+        last_input.layer.borderColor = UIColor.white.cgColor
+        last_input.layer.cornerRadius = 4.0
+        //last_input.resignFirstResponder()
+        view.addSubview(last_input)
+        
+        // from
         let from_label = UILabel()
-        from_label.frame = CGRect(x: 40, y: 160, width: 200, height: 20)
+        from_label.frame = CGRect(x: 40, y: 160, width: 200, height: 25)
         from_label.text = "From"
         from_label.textColor = .white
         view.addSubview(from_label)
         
-        let id_label = UITextField()
-        id_label.frame = CGRect(x: 40, y: 210, width: 200, height: 20)
-        id_label.backgroundColor = .white
-        id_label.layer.cornerRadius = 4.0
+        //let from_input = UITextField()
+        from_input.frame = CGRect(x: 150, y: 160, width: 180, height: 25)
+        from_input.backgroundColor = .black
+        from_input.textColor = .white
+        from_input.layer.borderWidth = 1
+        from_input.layer.borderColor = UIColor.white.cgColor
+        from_input.layer.cornerRadius = 4.0
+        //from_input.resignFirstResponder()
+        view.addSubview(from_input)
+        
+        // netid
+        let id_label = UILabel()
+        id_label.frame = CGRect(x: 40, y: 210, width: 200, height: 25)
+        id_label.text = "NetID"
+        id_label.textColor = .white
         view.addSubview(id_label)
-    
+        
+        // let id_input = UITextField()
+        id_input.frame = CGRect(x: 150, y: 210, width: 180, height: 25)
+        id_input.backgroundColor = .black
+        id_input.textColor = .white
+        id_input.layer.borderWidth = 1
+        id_input.layer.borderColor = UIColor.white.cgColor
+        id_input.layer.cornerRadius = 4.0
+        //id_input.resignFirstResponder()
+        view.addSubview(id_input)
+        
+        // segmented control to select gender
+        let gender_select = UISegmentedControl(items: ["Male", "Female"])
+        gender_select.frame = CGRect(x: 30, y: 260, width: 300, height: 25)
+        gender_select.backgroundColor = .gray
+        gender_select.selectedSegmentTintColor = .white
+        gender_select.addTarget(self, action: #selector(selectGender(_:)), for: .valueChanged)
+        view.addSubview(gender_select)
+        
+        // segmented control to select role
+        let role_select = UISegmentedControl(items: ["Prof", "TA", "Student"])
+        role_select.frame = CGRect(x: 30, y: 310, width: 300, height: 25)
+        role_select.backgroundColor = .gray
+        role_select.selectedSegmentTintColor = .white
+        role_select.addTarget(self, action: #selector(selectRole(_:)), for: .valueChanged)
+        view.addSubview(role_select)
+        
+        // segmented control to select program
+        let program_select = UISegmentedControl(items: ["Undergrad", "Grad", "NA"])
+        program_select.frame = CGRect(x: 30, y: 360, width: 300, height: 25)
+        program_select.backgroundColor = .gray
+        program_select.selectedSegmentTintColor = .white
+        program_select.addTarget(self, action: #selector(selectProgram(_:)), for: .valueChanged)
+        view.addSubview(program_select)
     }
     
 // You can add code here
+    // when change the gender segmented control
+    @objc func selectGender(_ gender_select: UISegmentedControl) {
+        switch (gender_select.selectedSegmentIndex) {
+        case 0:
+            selectedGender = Gender.Male
+            print(selectedGender!)
+        case 1:
+            selectedGender = Gender.Female
+            print(selectedGender!)
+        default:
+            break
+        }
+    }
+    
+    @objc func selectRole(_ role_select: UISegmentedControl) {
+        switch (role_select.selectedSegmentIndex) {
+        case 0:
+            selectedRole = DukeRole.Professor
+            print(selectedRole!)
+        case 1:
+            selectedRole = DukeRole.TA
+            print(selectedRole!)
+        case 2:
+            selectedRole = DukeRole.Student
+            print(selectedRole!)
+        default:
+            break
+        }
+    }
+    
+    @objc func selectProgram(_ program_select: UISegmentedControl) {
+        switch (program_select.selectedSegmentIndex) {
+        case 0:
+            selectedProgram = DukeProgram.Undergrad
+            print(selectedProgram!)
+        case 1:
+            selectedProgram = DukeProgram.Grad
+            print(selectedProgram!)
+        case 2:
+            selectedProgram = DukeProgram.NA
+            print(selectedProgram!)
+        default:
+            break
+        }
+    }
 
 }
 // Don't change the following line - it is what allowsthe view controller to show in the Live View window
