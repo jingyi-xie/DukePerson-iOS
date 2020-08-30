c3ec9d944b4286e64da5b2047636fe0be157fa49
jx95 <jx95@duke.edu>
2020-08-30 01:24:26 -0400

    add InformationViewController

---
+++ ECE564_HW/InformationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InformationViewController.swift
+//  ECE564_HW
+//
+//  Created by Jaryn on 2020/8/30.
+//  Copyright © 2020 ECE564. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
